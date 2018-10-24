2d7dfaf87fa3c2fb186d4d7b3b9b97728a544fa3
Alan <gogunov00@gmail.com>
2018-10-24 21:46:32 +0300

    fixes

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-function createFunc() {
-	let item = 10;
-	function increment() {
-		console.log(mark);
-	}
-	const mark = 'MArk';
-	return increment;
+function Word(string) {
+	this.str = string;
 }
 
-createFunc()();
+Word.prototype[Symbol.iterator] = function() {
+	let i = 0;
+	const array = this.str.split(' ');
+	return {
+		next() {
+			if(i < array.length) {
+				return {
+					value: array[i++],
+					done: false
+				}
+			} else {
+				return {
+					done: true
+				}
+			}
+
+		}
+	}
+};
+
+const res = new Word('My dear girl');
+
+for(let word of res) {
+	console.log(word);
+}
